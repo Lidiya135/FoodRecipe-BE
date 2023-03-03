16a1f697c349260aefe21ac0d868e6410b199ddb
Lidiya135 <lidiyawati315@gmail.com>
2023-03-03 18:01:05 +0700

    update

--- index.js
+++ index.js
@@ -32,6 +32,11 @@ app.use(
 );
 app.use(morgan("dev"));
 app.use(xss());
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// })
 
 app.use('/', mainRouter);
 app.use('/img', express.static('./upload'));
