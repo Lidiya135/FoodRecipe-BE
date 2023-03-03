99c44db3189c164dc485403c07a938927a38ed9b
Lidiya135 <lidiyawati315@gmail.com>
2023-03-03 11:48:41 +0700

    update

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ app.use(express.json()); app.use(express.urlencoded({
   })
  );
 app.use(bodyParser.json());
-app.use(cors())
+const corsOptions ={
+  origin:['http://localhost:3000', 'https://food-recipe-fe-mu.vercel.app/', 'https://food-recipe-fe-hbl9.vercel.app/' ],
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions))
 app.use(helmet.crossOriginResourcePolicy({policy:'cross-origin'})) 
 app.use(morgan("dev"));
 const port = process.env.PORT;
