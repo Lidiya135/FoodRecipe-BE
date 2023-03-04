47506d6bd601860e906c2c303c740b43e440fd3d
Lidiya135 <lidiyawati315@gmail.com>
2023-03-04 09:55:46 +0700

    update

--- src/middleware/common.js
+++ src/middleware/common.js
@@ -6,5 +6,13 @@ const response = (res, statusCode, status, result, message) => {
     printResult.message = message || null;
     res.status(statusCode).json(printResult);
   };
+
+  const resp = (res, statusCode, status, message) => {
+    const printResult = {};
+    printResult.success = status;
+    printResult.statusCode = statusCode;
+    printResult.message = message || null;
+    res.status(statusCode).json(printResult);
+  };
   
-  module.exports = { response };
+  module.exports = { response,  resp };
